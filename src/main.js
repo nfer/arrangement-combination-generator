dd98a2e7d2932f8f8ea2dd55bab26096644c3550
Nfer Zhuang <nfer.zhuang@gmail.com>
2020-12-10 16:34:37 +0800

    show build version

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue';
 import ElementUI from 'element-ui';
+import GitVersion from 'show-git-version';
 import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue';
+
+GitVersion.trace();
 
 Vue.use(ElementUI);
 
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,21 @@
+const GitRevisionPlugin = require('git-revision-webpack-plugin');
+const Package = require('./package.json');
+
 module.exports = {
   publicPath: '/arrangement-combination-generator',
+  chainWebpack: (config) => {
+    config.plugin('define').tap((definitions) => {
+      const gitRevisionPlugin = new GitRevisionPlugin({ branch: true });
+      const commit = gitRevisionPlugin.commithash().substr(0, 8);
+      const branch = gitRevisionPlugin.branch();
+      Object.assign(definitions[0]['process.env'], {
+        GIT_VERSION: JSON.stringify(`${branch}-${commit}`),
+        GIT_BRANCH: JSON.stringify(branch),
+        GIT_COMMIT: JSON.stringify(commit),
+        PROJ_VERSION: JSON.stringify(Package.version),
+        BUILD_TIME: JSON.stringify(new Date().toISOString()),
+      });
+      return definitions;
+    });
+  },
 };
